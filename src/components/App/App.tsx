da481c08b8ba5c5282a37160475779ea62c172d9
Oleksandr <netesa.sasha@gmail.com>
2025-08-03 22:28:59 +0300

    /s

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
-import { fetchMovies } from "../../services/movieServices";
+import { fetchMovies } from "../../services/movieService";
 
 import "./App.module.css";
 import { Movie } from "../../types/movie";
